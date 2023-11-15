99a860b084c5cdeb44173bcf5d875e42f23a66fc
vdircio <vdirci2@illinois.edu>
2023-11-15 09:07:36 -0600

    vendor desc

--- app/src/main/java/com/example/farmersmarket/VendorOne.java
+++ app/src/main/java/com/example/farmersmarket/VendorOne.java
@@ -1,13 +1,18 @@
 package com.example.farmersmarket;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
@@ -20,13 +25,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class VendorOne extends AppCompatActivity {
     boolean click = true;
+    private ImageButton fbBtn;
+    private ImageButton instBtn;
+    private ImageButton twitBtn;
+    private ImageButton emBtn;
+    private ImageButton gaBtn;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.vendor_one);
+
         ImageView imageView = (ImageView) findViewById(R.id.myImageView);
-        Button descriptionBtn = (Button) findViewById(R.id.description);
+        ImageButton descriptionBtn = (ImageButton) findViewById(R.id.description);
 
 
 //        PopupWindow popUp = new PopupWindow(this);
@@ -58,11 +68,46 @@ public class VendorOne extends AppCompatActivity {
 //        mainLayout.addView(but, params);
 //        setContentView(mainLayout);
 
+        fbBtn = (ImageButton) findViewById(R.id.facebook_btn);
+        instBtn = (ImageButton) findViewById(R.id.insta_btn);
+        twitBtn = (ImageButton) findViewById(R.id.twitter_btn);
+        emBtn = (ImageButton) findViewById(R.id.email_btn);
+        gaBtn = (ImageButton) findViewById(R.id.gallery_btn);
+        fbBtn.setOnClickListener(this::onClick);
+        instBtn.setOnClickListener(this::onClick);
+        twitBtn.setOnClickListener(this::onClick);
+        emBtn.setOnClickListener(this::onClick);
+        gaBtn.setOnClickListener(this::onClick);
+
         Spinner spinner_one = findViewById(R.id.spinner1);
         Spinner spinner_two = findViewById(R.id.spinner2);
         Spinner spinner_three = findViewById(R.id.spinner3);
         Spinner spinner_four = findViewById(R.id.spinner4);
         Spinner spinner_five = findViewById(R.id.spinner5);
 
+    }
+    public void onClick(View v) {
+        if (v.getId() == R.id.facebook_btn) {
+            Uri fbPage = Uri.parse("https://www.facebook.com/");
+            Intent intent1 = new Intent();
+            intent1.setAction(Intent.ACTION_VIEW);
+            intent1.setData(fbPage);
+            Intent chooser1 = Intent.createChooser(intent1, "OPEN WITH");
+            startActivity(chooser1);
+        } else if (v.getId() == R.id.insta_btn) {
+            Uri instPage = Uri.parse("https://www.instagram.com/");
+            Intent intent2 = new Intent();
+            intent2.setAction(Intent.ACTION_VIEW);
+            intent2.setData(instPage);
+            Intent chooser2 = Intent.createChooser(intent2, "OPEN WITH");
+            startActivity(chooser2);
+        } else if (v.getId() == R.id.twitter_btn) {
+            Uri twitPage = Uri.parse("https://twitter.com/?lang=en");
+            Intent intent3 = new Intent();
+            intent3.setAction(Intent.ACTION_VIEW);
+            intent3.setData(twitPage);
+            Intent chooser3 = Intent.createChooser(intent3, "OPEN WITH");
+            startActivity(chooser3);
+        }
     }
 }
