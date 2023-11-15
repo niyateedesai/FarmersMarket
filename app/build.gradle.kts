df44e78297f5b681e177bccdd5c8987aefcdc516
PasserBo <PasserBo>
2023-11-15 14:43:34 -0600

    Made an example homepage of the app, all the warnings in file now is string related issues, which can be fixed later

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.farmersmarket"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.farmersmarket"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
